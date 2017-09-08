81d5080153e861fab4492f6d9be05af3cd6ba23c
Dilek Uzulmez <dilekuzulmez@gmail.com>
2017-09-08 11:27:09 +0300

    first commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+use Illuminate\Http\Request;
+
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+       // $this->middleware('guest');
+    }
+
+    /*
+     *Kullanılmayan fonksiyon
+    protected function register(Request $request)
+    {
+        $inputs =  $request->only('name', 'email', 'password');
+        //dd($inputs);
+        return $this->create($inputs);
+    }
+    */
+
+    public function register(Request $request)
+    {
+        // grab credentials from the request
+        $user = User::where('email', '=', $request->get('email'))->first();
+       // dd($credentials);
+        try {
+            $token = JWTAuth::fromUser($user);
+            // attempt to verify the credentials and create a token for the user
+            if (! $token ) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // all good so return the token
+        return response()->json(['status' => true, 'data' => compact('token')]);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+}
